e1e1d211bd49868a4d9a04b623e6d9b8706cf2a5
Oleh Karapysh <sebastianvell777@gmail.com>
2026-03-29 13:42:51 +0300

    Setup Web API project

---
+++ src/NeuroNotes.WebApi/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.MapGet("/", () => "Server is running");
+
+await app.RunAsync();
